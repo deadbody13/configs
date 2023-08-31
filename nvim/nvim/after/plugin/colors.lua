1c7a8f2317b1aeeac421634b1e5ce328d621f78c
sven <nate25381@gmail.com>
2023-08-31 11:52:27 -0400

    created nvim directory with setup script

---
+++ nvim/nvim/after/plugin/colors.lua
@@ -0,0 +1,6 @@
+function ColorMyPencils(color)
+	color = color or "github_light"
+	vim.cmd.colorscheme(color)
+end
+
+ColorMyPencils()
---
+++ nvim/nvim/lua/dead/init.lua
@@ -0,0 +1,7 @@
+require("dead.remap")
+require ("dead.set")
+require("dead.packer")
+
+if vim.g.neovide then
+	require("dead.neovide")
+end
