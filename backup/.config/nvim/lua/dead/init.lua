9219dd7fe968a63a65904354bc452919c30aa5ed
sven <nate25381@gmail.com>
2024-03-24 17:06:26 -0400

    updated current and pictochat themes

--- backup/.config/nvim/lua/dead/init.lua
+++ backup/.config/nvim/lua/dead/init.lua
@@ -2,7 +2,8 @@ require("dead.remap")
 require ("dead.set")
 require("dead.packer")
 require("dead.barbar")
-
+-- require("dead.lualine")
+require("dead.lualine")
 if vim.g.neovide then
 	require("dead.neovide")
 end
--- backup/.config/nvim/plugin/packer_compiled.lua
+++ backup/.config/nvim/plugin/packer_compiled.lua
@@ -89,12 +89,6 @@ _G.packer_plugins = {
     path = "/home/sven/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
-  ["everforest-nvim"] = {
-    config = { "\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15everforest\frequire\0" },
-    loaded = true,
-    path = "/home/sven/.local/share/nvim/site/pack/packer/start/everforest-nvim",
-    url = "https://github.com/neanias/everforest-nvim"
-  },
   ["gitsigns.nvim"] = {
     loaded = true,
     path = "/home/sven/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
@@ -183,10 +177,6 @@ time([[Defining packer_plugins]], false)
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
 time([[Config for nvim-autopairs]], false)
--- Config for: everforest-nvim
-time([[Config for everforest-nvim]], true)
-try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\15everforest\frequire\0", "config", "everforest-nvim")
-time([[Config for everforest-nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
