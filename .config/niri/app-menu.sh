235400fc262e92f5b37a105e8c9b64c6a7500d8b
sven <nate25381@gmail.com>
2026-01-18 14:51:52 -0500

    updated fuzzel icons

--- .config/niri/app-menu.sh
+++ .config/niri/app-menu.sh
@@ -2,11 +2,11 @@
 
 pkill fuzzel
 
-input=$(printf "Firefox\0icon\x1ffirefox\nDiscord\0icon\x1fdiscord\nSteam\0icon\x1fsteam" | fuzzel --dmenu --icon-theme=Newaita-replacement --minimal-lines --y-margin=110 --hide-prompt --width=15)
+input=$(printf "Zen\0icon\x1fweb-browser\nDiscord\0icon\x1fdiscord\nSteam\0icon\x1fsteam" | fuzzel --dmenu --icon-theme=WhiteSur --minimal-lines --y-margin=110 --hide-prompt --width=15)
 
 case "$input" in
-	Firefox)
-		firefox
+	Zen)
+		flatpak run app.zen_browser.zen
 		;;
 	Discord)
 		flatpak run com.discordapp.Discord
--- .config/niri/power.sh
+++ .config/niri/power.sh
@@ -2,7 +2,7 @@
 
 pkill fuzzel
 
-input=$(printf "suspend\0icon\x1fsystem-suspend\nshutdown\0icon\x1fsystem-shutdown\nreboot\0icon\x1fsystem-reboot" | fuzzel --dmenu --icon-theme=Newaita-replacement --minimal-lines --y-margin=70 --hide-prompt --width=15)
+input=$(printf "suspend\0icon\x1fsystem-suspend\nshutdown\0icon\x1fsystem-shutdown\nreboot\0icon\x1fsystem-reboot" | fuzzel --dmenu --icon-theme=WhiteSur --minimal-lines --y-margin=70 --hide-prompt --width=15)
 
 case "$input" in
 	suspend)
